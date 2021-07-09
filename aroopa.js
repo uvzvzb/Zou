47b4238d6ad8f66493c7c568c9b0361eac0da267
uvzvzb <87128403+uvzvzb@users.noreply.github.com>
2021-07-09 22:07:33 +0530

    Add files via upload

---
+++ aroopa.js
@@ -0,0 +1,25 @@
+
+var cricle = document.querySelector(".cr1");
+
+cricle.addEventListener("click",()=>{
+    document.getElementById("text").innerText = "Select your farmshare and complete reservation form. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda mollitia, voluptates obcaecati"
+});
+
+
+var cricle = document.querySelector(".cr2");
+
+cricle.addEventListener("click",()=>{
+    document.getElementById("text").innerText = "Swallowed a planet!.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda mollitia, voluptates obcaecati molestias quod velit!"
+});
+
+var cricle = document.querySelector(".cr3");
+
+cricle.addEventListener("click",()=>{
+    document.getElementById("text").innerText = "It's art! A statement  Father Christmas. Santa Claus. Or as I've always known him: Jeff. Sorry, checking all the water in this area; there's an escaped fish. "
+});
+
+var cricle = document.querySelector(".cr4");
+
+cricle.addEventListener("click",()=>{
+    document.getElementById("text").innerText = "It's art! A statement on modern society, 'Oh Ain't Modern Society Awful?'! I am the Doctor, and you are the Daleks! Stop talking, brain thinking.  "
+});
